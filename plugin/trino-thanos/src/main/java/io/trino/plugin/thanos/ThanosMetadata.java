a39cb311904ef6027eb67adec0e740013f6c828e
Anish Asthana <anishasthana1@gmail.com>
2021-12-02 16:29:59 -0500

    Complete Thanos Metadata
    Signed-off-by: Anish Asthana <anishasthana1@gmail.com>

--- plugin/trino-thanos/src/main/java/io/trino/plugin/thanos/ThanosMetadata.java
+++ plugin/trino-thanos/src/main/java/io/trino/plugin/thanos/ThanosMetadata.java
@@ -14,14 +14,17 @@
 package io.trino.plugin.thanos;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
-import io.trino.spi.connector.ConnectorMetadata;
-import io.trino.spi.connector.ConnectorSession;
-import io.trino.spi.connector.SchemaTableName;
+import io.trino.plugin.prometheus.PrometheusColumnHandle;
+import io.trino.plugin.prometheus.PrometheusTable;
+import io.trino.plugin.prometheus.PrometheusTableHandle;
+import io.trino.spi.connector.*;
 
 import javax.inject.Inject;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 
@@ -51,6 +54,26 @@ public final class ThanosMetadata
     }
 
     @Override
+    public PrometheusTableHandle getTableHandle(ConnectorSession session, SchemaTableName tableName)
+    {
+        if (!listSchemaNames(session).contains(tableName.getSchemaName())) {
+            return null;
+        }
+
+        if (thanosClient.getTable(tableName.getSchemaName(), tableName.getTableName()) == null) {
+            return null;
+        }
+
+        return new PrometheusTableHandle(tableName.getSchemaName(), tableName.getTableName());
+    }
+
+    @Override
+    public ConnectorTableMetadata getTableMetadata(ConnectorSession session, ConnectorTableHandle table)
+    {
+        return getTableMetadata(((PrometheusTableHandle) table).toSchemaTableName());
+    }
+
+    @Override
     public List<SchemaTableName> listTables(ConnectorSession session, Optional<String> optionalSchemaName)
     {
         Set<String> schemaNames = optionalSchemaName.map(ImmutableSet::of)
@@ -61,4 +84,87 @@ public final class ThanosMetadata
                         thanosClient.getTableNames(schemaName).stream().map(tableName -> new SchemaTableName(schemaName, tableName)))
                 .collect(toImmutableList());
     }
+
+    @Override
+    public Map<String, ColumnHandle> getColumnHandles(ConnectorSession session, ConnectorTableHandle tableHandle)
+    {
+        PrometheusTableHandle prometheusTableHandle = (PrometheusTableHandle) tableHandle;
+
+        PrometheusTable table = thanosClient.getTable(prometheusTableHandle.getSchemaName(), prometheusTableHandle.getTableName());
+        if (table == null) {
+            throw new TableNotFoundException(prometheusTableHandle.toSchemaTableName());
+        }
+
+        ImmutableMap.Builder<String, ColumnHandle> columnHandles = ImmutableMap.builder();
+        int index = 0;
+        for (ColumnMetadata column : table.getColumnsMetadata()) {
+            columnHandles.put(column.getName(), new PrometheusColumnHandle(column.getName(), column.getType(), index));
+            index++;
+        }
+        return columnHandles.build();
+    }
+
+    @Override
+    public Map<SchemaTableName, List<ColumnMetadata>> listTableColumns(ConnectorSession session, SchemaTablePrefix prefix)
+    {
+        requireNonNull(prefix, "prefix is null");
+        ImmutableMap.Builder<SchemaTableName, List<ColumnMetadata>> columns = ImmutableMap.builder();
+        for (SchemaTableName tableName : listTables(session, prefix)) {
+            ConnectorTableMetadata tableMetadata = getTableMetadata(tableName);
+            // table can disappear during listing operation
+            if (tableMetadata != null) {
+                columns.put(tableName, tableMetadata.getColumns());
+            }
+        }
+        return columns.build();
+    }
+
+    private ConnectorTableMetadata getTableMetadata(SchemaTableName tableName)
+    {
+        if (!listSchemaNames().contains(tableName.getSchemaName())) {
+            return null;
+        }
+
+        PrometheusTable table = thanosClient.getTable(tableName.getSchemaName(), tableName.getTableName());
+        if (table == null) {
+            return null;
+        }
+
+        return new ConnectorTableMetadata(tableName, table.getColumnsMetadata());
+    }
+
+    private List<SchemaTableName> listTables(ConnectorSession session, SchemaTablePrefix prefix)
+    {
+        if (prefix.getTable().isEmpty()) {
+            return listTables(session, prefix.getSchema());
+        }
+        return ImmutableList.of(prefix.toSchemaTableName());
+    }
+
+    @Override
+    public ColumnMetadata getColumnMetadata(ConnectorSession session, ConnectorTableHandle tableHandle, ColumnHandle columnHandle)
+    {
+        return ((PrometheusColumnHandle) columnHandle).getColumnMetadata();
+    }
+
+    @Override
+    public boolean usesLegacyTableLayouts()
+    {
+        return false;
+    }
+
+    @Override
+    public ConnectorTableProperties getTableProperties(ConnectorSession session, ConnectorTableHandle table)
+    {
+        return new ConnectorTableProperties();
+    }
+
+    @Override
+    public Optional<ConstraintApplicationResult<ConnectorTableHandle>> applyFilter(ConnectorSession session, ConnectorTableHandle handle, Constraint constraint)
+    {
+        PrometheusTableHandle tableHandle = ((PrometheusTableHandle) handle)
+                .withPredicate(constraint.getSummary());
+        return Optional.of(new ConstraintApplicationResult<>(tableHandle, constraint.getSummary(), false));
+    }
+
 }
